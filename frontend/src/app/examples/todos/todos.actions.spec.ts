cd6b9ba92ce75e2b6e05aaeb3141831e8a381712
Valery Lobachev <valery@lobachyov.com>
2018-08-14 18:47:52 +0300

    feat(frontend application): initial commit of frontend application (angular 6 + ngrx + material)

---
+++ frontend/src/app/examples/todos/todos.actions.spec.ts
@@ -0,0 +1,59 @@
+import * as fromTodos from './todos.reducer';
+
+describe('Todos Actions', () => {
+  describe('TodosAdd', () => {
+    it('should create an action', () => {
+      const action = new fromTodos.ActionTodosAdd({ name: 'test' });
+      expect(action.payload).toEqual(
+        jasmine.objectContaining({
+          name: 'test'
+        })
+      );
+      expect(action.type).toEqual(fromTodos.TodosActionTypes.ADD);
+      expect(action.payload.id).toBeDefined();
+    });
+  });
+
+  describe('ActionTodosToggle', () => {
+    it('should create an action', () => {
+      const action = new fromTodos.ActionTodosToggle({ id: '1' });
+
+      expect({ ...action }).toEqual({
+        type: fromTodos.TodosActionTypes.TOGGLE,
+        payload: { id: '1' }
+      });
+    });
+  });
+
+  describe('ActionTodosRemoveDone', () => {
+    it('should create an action', () => {
+      const action = new fromTodos.ActionTodosRemoveDone();
+
+      expect({ ...action }).toEqual({
+        type: fromTodos.TodosActionTypes.REMOVE_DONE
+      });
+    });
+  });
+
+  describe('ActionTodosFilter', () => {
+    it('should create an action', () => {
+      const action = new fromTodos.ActionTodosFilter({ filter: 'DONE' });
+
+      expect({ ...action }).toEqual({
+        type: fromTodos.TodosActionTypes.FILTER,
+        payload: { filter: 'DONE' }
+      });
+    });
+  });
+
+  describe('ActionTodosPersist', () => {
+    it('should create an action', () => {
+      const action = new fromTodos.ActionTodosPersist({ todos: [] });
+
+      expect({ ...action }).toEqual({
+        type: fromTodos.TodosActionTypes.PERSIST,
+        payload: { todos: [] }
+      });
+    });
+  });
+});
